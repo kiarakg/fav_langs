afdd5809fa164a62a25d29d886e5be68dbbe174e
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-09 12:09:07 -0400

    user input added

--- fav_langs.py
+++ fav_langs.py
@@ -1,11 +1,19 @@
 data_scientist = {
-    "name" : None,
-    "age" : None,
-    "years" : None
+    "Name" : None,
+    "Age" : None,
+    "Years" : None
 }
 
-data_scientist["name"] = input("Please enter you name: ")
-data_scientist["age"] = input("Please enter you age: ")
-data_scientist["years"] = input("How long have you been coding? ")
+data_scientist["Name"] = input("Please enter you name: ")
+data_scientist["Age"] = input("Please enter you age: ")
+data_scientist["Years"] = input("How long have you been coding? ")
+
+print(data_scientist)
+
+first_languages = input("Enter the first three programming language you have learned (separated by commas): ")
+lang_list = first_languages.split(",")
+lang_tuple = tuple (lang_list)
+
+data_scientist["First Languages"] = lang_tuple
 
 print(data_scientist)
