813ba395e7b7606ac80f4789a83388c79f5a00ad
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-09 11:13:46 -0400

    dictionary created

---
+++ fav_langs.py
@@ -0,0 +1,11 @@
+data_scientist = {
+    "name" : None,
+    "age" : None,
+    "years" : None
+}
+
+data_scientist["name"] = input("Please enter you name: ")
+data_scientist["age"] = input("Please enter you age: ")
+data_scientist["years"] = input("How long have you been coding? ")
+
+print(data_scientist)
